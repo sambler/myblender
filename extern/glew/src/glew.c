4bcf429d85f5d3955294fb12c225f6a355bac7c3
SAmbler <Develop@ShaneWare.Biz>
2010-12-09 13:42:17 +1030

    use guardedalloc - glew

--- extern/glew/src/glew.c
+++ extern/glew/src/glew.c
@@ -36,6 +36,8 @@
 #  include <GL/glxew.h>
 #endif
 
+#include "MEM_guardedalloc.h"
+
 /*
  * Define glewGetContext and related helper macros.
  */
@@ -80,14 +82,14 @@ void* NSGLGetProcAddress (const GLubyte *name)
     image = NSAddImage("/System/Library/Frameworks/OpenGL.framework/Versions/Current/OpenGL", NSADDIMAGE_OPTION_RETURN_ON_ERROR);
   }
   /* prepend a '_' for the Unix C symbol mangling convention */
-  symbolName = malloc(strlen((const char*)name) + 2);
+  symbolName = MEM_mallocN(strlen((const char*)name) + 2,"glew-NSGLGetProcAddress");
   strcpy(symbolName+1, (const char*)name);
   symbolName[0] = '_';
   symbol = NULL;
   /* if (NSIsSymbolNameDefined(symbolName))
 	 symbol = NSLookupAndBindSymbol(symbolName); */
   symbol = image ? NSLookupSymbolInImage(image, symbolName, NSLOOKUPSYMBOLINIMAGE_OPTION_BIND | NSLOOKUPSYMBOLINIMAGE_OPTION_RETURN_ON_ERROR) : NULL;
-  free(symbolName);
+  MEM_freeN(symbolName);
   return symbol ? NSAddressOfSymbol(symbol) : NULL;
 }
 #endif /* __APPLE__ */

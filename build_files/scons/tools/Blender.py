1dffba1a48cc1cae0a2c95b5e2718f99633d81b7
SAmbler <Develop@Shaneware.Biz>
2011-08-18 01:32:44 +0930

    svn update

--- build_files/scons/tools/Blender.py
+++ build_files/scons/tools/Blender.py
@@ -350,7 +350,7 @@ def buildinfo(lenv, build_type):
     build_time = time.strftime ("%H:%M:%S")
     build_rev = os.popen('svnversion').read()[:-1] # remove \n
     if build_rev == '': 
-        build_rev = '39425'
+        build_rev = '39496'
     if lenv['BF_DEBUG']:
         build_type = "Debug"
         build_cflags = ' '.join(lenv['CFLAGS'] + lenv['CCFLAGS'] + lenv['BF_DEBUG_CCFLAGS'] + lenv['CPPFLAGS'])

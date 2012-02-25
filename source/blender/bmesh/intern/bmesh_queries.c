sambler/myblender

commit e36d36afa05ce421926752be82e72d4f8de55e78

bmesh minor api refactor * BM_vert_in_face now loops over a vert's faces rather then every vert in the face. * rename bmesh_radial_find_first_facevert --> bmesh_radial_find_first_faceloop * rename bmesh_radial_find_next_facevert --> bmesh_radial_find_next_faceloop * rename BM_vert_collapse_edges --> BM_vert_collapse_edge (only collapses one)
git-svn-id: https://svn.blender.org/svnroot/bf-blender/trunk/blender@44361 954f8c5b-7b00-dc11-b283-0030488c597c

source/blender/bmesh/intern/bmesh_queries.c
@@ -101,26 +101,7 @@ BMLoop *BM_face_other_loop(BMEdge *e, BMFace *f, BMVert *v)
 
 int BM_vert_in_face(BMFace *f, BMVert *v)
 {
-	BMLoop *l_iter, *l_first;
-
-#ifdef USE_BMESH_HOLES
-	BMLoopList *lst;
-	for (lst = f->loops.first; lst; lst = lst->next)
-#endif
-	{
-#ifdef USE_BMESH_HOLES
-		l_iter = l_first = lst->first;
-#else
-		l_iter = l_first = f->l_first;
-#endif
-		do {
-			if (l_iter->v == v) {
-				return TRUE;
-			}
-		} while ((l_iter = l_iter->next) != l_first);
-	}
-
-	return FALSE;
+	return bmesh_radial_find_first_faceloop(BM_FACE_FIRST_LOOP(f), v) != NULL;
 }
 
 /*

source/blender/bmesh/bmesh.h
@@ -216,7 +216,7 @@ BMFace *BM_face_split(BMesh *bm, BMFace *f,
 
 /* these 2 functions are very similar */
 BMEdge* BM_vert_collapse_faces(BMesh *bm, BMEdge *ke, BMVert *kv, float fac, const int join_faces);
-BMEdge* BM_vert_collapse_edges(BMesh *bm, BMEdge *ke, BMVert *kv);
+BMEdge* BM_vert_collapse_edge(BMesh *bm, BMEdge *ke, BMVert *kv);
 
 
 /* splits an edge.  ne is set to the new edge created. */

source/blender/bmesh/intern/bmesh_iterators.c
@@ -200,7 +200,7 @@ void  bmiter__face_of_vert_begin(BMIter *iter)
 	if (iter->count) {
 		iter->firstedge = bmesh_disk_find_first_faceedge(iter->vdata->e, iter->vdata);
 		iter->nextedge = iter->firstedge;
-		iter->firstloop = bmesh_radial_find_first_facevert(iter->firstedge->l, iter->vdata);
+		iter->firstloop = bmesh_radial_find_first_faceloop(iter->firstedge->l, iter->vdata);
 		iter->nextloop = iter->firstloop;
 	}
 }
@@ -210,10 +210,10 @@ void  *bmiter__face_of_vert_step(BMIter *iter)
 
 	if (iter->count && iter->nextloop) {
 		iter->count--;
-		iter->nextloop = bmesh_radial_find_next_facevert(iter->nextloop, iter->vdata);
+		iter->nextloop = bmesh_radial_find_next_faceloop(iter->nextloop, iter->vdata);
 		if (iter->nextloop == iter->firstloop) {
 			iter->nextedge = bmesh_disk_find_next_faceedge(iter->nextedge, iter->vdata);
-			iter->firstloop = bmesh_radial_find_first_facevert(iter->nextedge->l, iter->vdata);
+			iter->firstloop = bmesh_radial_find_first_faceloop(iter->nextedge->l, iter->vdata);
 			iter->nextloop = iter->firstloop;
 		}
 	}
@@ -238,7 +238,7 @@ void  bmiter__loop_of_vert_begin(BMIter *iter)
 	if (iter->count) {
 		iter->firstedge = bmesh_disk_find_first_faceedge(iter->vdata->e, iter->vdata);
 		iter->nextedge = iter->firstedge;
-		iter->firstloop = bmesh_radial_find_first_facevert(iter->firstedge->l, iter->vdata);
+		iter->firstloop = bmesh_radial_find_first_faceloop(iter->firstedge->l, iter->vdata);
 		iter->nextloop = iter->firstloop;
 	}
 }
@@ -248,10 +248,10 @@ void  *bmiter__loop_of_vert_step(BMIter *iter)
 
 	if (iter->count) {
 		iter->count--;
-		iter->nextloop = bmesh_radial_find_next_facevert(iter->nextloop, iter->vdata);
+		iter->nextloop = bmesh_radial_find_next_faceloop(iter->nextloop, iter->vdata);
 		if (iter->nextloop == iter->firstloop) {
 			iter->nextedge = bmesh_disk_find_next_faceedge(iter->nextedge, iter->vdata);
-			iter->firstloop = bmesh_radial_find_first_facevert(iter->nextedge->l, iter->vdata);
+			iter->firstloop = bmesh_radial_find_first_faceloop(iter->nextedge->l, iter->vdata);
 			iter->nextloop = iter->firstloop;
 		}
 	}

source/blender/bmesh/intern/bmesh_mods.c
@@ -307,7 +307,7 @@ BMEdge *BM_verts_connect(BMesh *bm, BMVert *v1, BMVert *v2, BMFace **nf)
 	BMLoop *nl;
 	BMFace *face;
 
-	/* be warned: this can do weird things in some ngon situation, see BM_LegalSplit */
+	/* be warned: this can do weird things in some ngon situation, see BM_face_legal_splits */
 	for (face = BM_iter_new(&iter, bm, BM_FACES_OF_VERT, v1); face; face = BM_iter_step(&iter)) {
 		for (v = BM_iter_new(&iter2, bm, BM_VERTS_OF_FACE, face); v; v = BM_iter_step(&iter2)) {
 			if (v == v2) {
@@ -344,7 +344,9 @@ BMFace *BM_face_split(BMesh *bm, BMFace *f, BMVert *v1, BMVert *v2, BMLoop **nl,
 {
 	const int has_mdisp = CustomData_has_layer(&bm->ldata, CD_MDISPS);
 	BMFace *nf, *of;
-	
+
+	BLI_assert(v1 != v2);
+
 	/* do we have a multires layer */
 	if (has_mdisp) {
 		of = BM_face_copy(bm, f, FALSE, FALSE);
@@ -397,7 +399,7 @@ BMFace *BM_face_split(BMesh *bm, BMFace *f, BMVert *v1, BMVert *v2, BMLoop **nl,
  *
  *  Note that this is not a general edge collapse function.
  *
- * Note this function is very close to 'BM_vert_collapse_edges', both collapse
+ * Note this function is very close to 'BM_vert_collapse_edge', both collapse
  * a vertex and return a new edge. Except this takes a factor and merges
  * custom data.
  *
@@ -476,7 +478,7 @@ BMEdge *BM_vert_collapse_faces(BMesh *bm, BMEdge *ke, BMVert *kv, float fac, con
 	}
 
 	/* single face or no faces */
-	/* same as BM_vert_collapse_edges() however we already
+	/* same as BM_vert_collapse_edge() however we already
 	 * have vars to perform this operation so dont call. */
 	bmesh_jekv(bm, ke, kv);
 	ne = BM_edge_exists(tv, tv2);
@@ -486,7 +488,7 @@ BMEdge *BM_vert_collapse_faces(BMesh *bm, BMEdge *ke, BMVert *kv, float fac, con
 
 
 /**
- *			BM_vert_collapse_edges
+ *			BM_vert_collapse_edge
  *
  * Collapses a vertex onto another vertex it shares an edge with.
  *
@@ -494,7 +496,7 @@ BMEdge *BM_vert_collapse_faces(BMesh *bm, BMEdge *ke, BMVert *kv, float fac, con
  * The New Edge
  */
 
-BMEdge *BM_vert_collapse_edges(BMesh *bm, BMEdge *ke, BMVert *kv)
+BMEdge *BM_vert_collapse_edge(BMesh *bm, BMEdge *ke, BMVert *kv)
 {
 	/* nice example implimentation but we want loops to have their customdata
 	 * accounted for */

source/blender/bmesh/intern/bmesh_structure.c
@@ -339,21 +339,21 @@ struct BMEdge *bmesh_disk_find_next_faceedge(struct BMEdge *e, struct BMVert *v)
 /*****radial cycle functions, e.g. loops surrounding edges**** */
 int bmesh_radial_validate(int radlen, BMLoop *l)
 {
-	BMLoop *l2 = l;
+	BMLoop *l_iter = l;
 	int i = 0;
 	
 	if (bmesh_radial_length(l) != radlen)
 		return FALSE;
 
 	do {
-		if (!l2) {
+		if (!l_iter) {
 			bmesh_error();
 			return FALSE;
 		}
 		
-		if (l2->e != l->e)
+		if (l_iter->e != l->e)
 			return FALSE;
-		if (l2->v != l->e->v1 && l2->v != l->e->v2)
+		if (l_iter->v != l->e->v1 && l_iter->v != l->e->v2)
 			return FALSE;
 		
 		if (i > BM_LOOP_RADIAL_MAX) {
@@ -362,8 +362,7 @@ int bmesh_radial_validate(int radlen, BMLoop *l)
 		}
 		
 		i++;
-		l2 = l2->radial_next;
-	} while (l2 != l);
+	} while ((l_iter = bmesh_radial_nextloop(l_iter)) != l);
 
 	return TRUE;
 }
@@ -414,31 +413,27 @@ void bmesh_radial_remove_loop(BMLoop *l, BMEdge *e)
  * Finds the first loop of v around radial
  * cycle
  */
-BMLoop *bmesh_radial_find_first_facevert(BMLoop *l, BMVert *v)
+BMLoop *bmesh_radial_find_first_faceloop(BMLoop *l, BMVert *v)
 {
-	BMLoop *curloop;
-	curloop = l;
+	BMLoop *l_iter;
+	l_iter = l;
 	do {
-		if (curloop->v == v) {
-			return curloop;
+		if (l_iter->v == v) {
+			return l_iter;
 		}
-
-		curloop = bmesh_radial_nextloop(curloop);
-	} while (curloop != l);
+	} while ((l_iter = bmesh_radial_nextloop(l_iter)) != l);
 	return NULL;
 }
 
-BMLoop *bmesh_radial_find_next_facevert(BMLoop *l, BMVert *v)
+BMLoop *bmesh_radial_find_next_faceloop(BMLoop *l, BMVert *v)
 {
-	BMLoop *curloop;
-	curloop = bmesh_radial_nextloop(l);
+	BMLoop *l_iter;
+	l_iter = bmesh_radial_nextloop(l);
 	do {
-		if (curloop->v == v) {
-			return curloop;
+		if (l_iter->v == v) {
+			return l_iter;
 		}
-
-		curloop = bmesh_radial_nextloop(curloop);
-	} while (curloop != l);
+	} while ((l_iter = bmesh_radial_nextloop(l_iter)) != l);
 	return l;
 }
 
@@ -520,13 +515,15 @@ int bmesh_radial_find_face(BMEdge *e, BMFace *f)
 
 int bmesh_radial_count_facevert(BMLoop *l, BMVert *v)
 {
-	BMLoop *curloop;
+	BMLoop *l_iter;
 	int count = 0;
-	curloop = l;
+	l_iter = l;
 	do {
-		if (curloop->v == v) count++;
-		curloop = bmesh_radial_nextloop(curloop);
-	} while (curloop != l);
+		if (l_iter->v == v) {
+			count++;
+		}
+	} while ((l_iter = bmesh_radial_nextloop(l_iter)) != l);
+
 	return count;
 }
 
@@ -918,10 +915,6 @@ BMEdge *bmesh_disk_existedge(BMVert *v1, BMVert *v2)
 }
 
 /* end disk cycle routine */
-BMLoop *bmesh_radial_nextloop(BMLoop *l)
-{
-	return l->radial_next;
-}
 
 void bmesh_radial_append(BMEdge *e, BMLoop *l)
 {
@@ -1017,7 +1010,7 @@ int bmesh_disk_count_facevert(BMVert *v)
  * cycle
  *
  */
-BMLoop *bmesh_radial_find_first_facevert(BMLoop *l, BMVert *v)
+BMLoop *bmesh_radial_find_first_faceloop(BMLoop *l, BMVert *v)
 {
 	BMLoop *curloop;
 	curloop = l;
@@ -1031,7 +1024,7 @@ BMLoop *bmesh_radial_find_first_facevert(BMLoop *l, BMVert *v)
 	return NULL;
 }
 
-BMLoop *bmesh_radial_find_next_facevert(BMLoop *l, BMVert *v)
+BMLoop *bmesh_radial_find_next_faceloop(BMLoop *l, BMVert *v)
 {
 	BMLoop *curloop;
 	curloop = bmesh_radial_nextloop(l);

source/blender/bmesh/intern/bmesh_structure.h
@@ -43,7 +43,7 @@ struct ListBase;
 
 void remove_loop_radial_link(BMLoop *l);
 
-/*DOUBLE CIRCULAR LINKED LIST FUNCTIONS*/
+/* DOUBLE CIRCULAR LINKED LIST FUNCTIONS */
 void bmesh_cycle_append(void *h, void *nt);
 int bmesh_cycle_remove(void *h, void *remn);
 int bmesh_cycle_validate(int len, void *h);
@@ -52,7 +52,7 @@ int bmesh_cycle_length(void *h);
 /* LOOP CYCLE MANAGEMENT */
 int bmesh_loop_validate(BMFace *f);
 
-/*DISK CYCLE MANAGMENT*/
+/* DISK CYCLE MANAGMENT */
 int bmesh_disk_append_edge(struct BMEdge *e, struct BMVert *v);
 void bmesh_disk_remove_edge(struct BMEdge *e, struct BMVert *v);
 struct BMEdge *bmesh_disk_nextedge(struct BMEdge *e, struct BMVert *v);
@@ -61,17 +61,17 @@ int bmesh_disk_count_facevert(struct BMVert *v);
 struct BMEdge *bmesh_disk_find_first_faceedge(struct BMEdge *e, struct BMVert *v);
 struct BMEdge *bmesh_disk_find_next_faceedge(struct BMEdge *e, struct BMVert *v);
 
-/*RADIAL CYCLE MANAGMENT*/
+/* RADIAL CYCLE MANAGMENT */
 void bmesh_radial_append(struct BMEdge *e, struct BMLoop *l);
 void bmesh_radial_remove_loop(struct BMLoop *l, struct BMEdge *e);
 int bmesh_radial_find_face(struct BMEdge *e, struct BMFace *f);
 struct BMLoop *bmesh_radial_nextloop(struct BMLoop *l);
 int bmesh_radial_count_facevert(struct BMLoop *l, struct BMVert *v);
-struct BMLoop *bmesh_radial_find_first_facevert(struct BMLoop *l, struct BMVert *v);
-struct BMLoop *bmesh_radial_find_next_facevert(struct BMLoop *l, struct BMVert *v);
+struct BMLoop *bmesh_radial_find_first_faceloop(struct BMLoop *l, struct BMVert *v);
+struct BMLoop *bmesh_radial_find_next_faceloop(struct BMLoop *l, struct BMVert *v);
 int bmesh_radial_validate(int radlen, struct BMLoop *l);
 
-/*EDGE UTILITIES*/
+/* EDGE UTILITIES */
 int bmesh_vert_in_edge(struct BMEdge *e, struct BMVert *v);
 int bmesh_verts_in_edge(struct BMVert *v1, struct BMVert *v2, struct BMEdge *e);
 int bmesh_edge_swapverts(struct BMEdge *e, struct BMVert *orig, struct BMVert *newv); /*relink edge*/

source/blender/bmesh/operators/bmo_dissolve.c
@@ -170,7 +170,7 @@ void dissolvefaces_exec(BMesh *bm, BMOperator *op)
 		BM_ITER(v, &viter, bm, BM_VERTS_OF_MESH, NULL) {
 			if (BMO_elem_flag_test(bm, v, VERT_MARK)) {
 				if (BM_vert_edge_count(v) == 2) {
-					BM_vert_collapse_edges(bm, v->e, v);
+					BM_vert_collapse_edge(bm, v->e, v);
 				}
 			}
 		}
@@ -221,7 +221,7 @@ void dissolve_edgeloop_exec(BMesh *bm, BMOperator *op)
 	/* clean up extreneous 2-valence vertice */
 	for (i = 0; i < BLI_array_count(verts); i++) {
 		if (verts[i]->e) {
-			BM_vert_collapse_edges(bm, verts[i]->e, verts[i]);
+			BM_vert_collapse_edge(bm, verts[i]->e, verts[i]);
 		}
 	}
 	
@@ -275,7 +275,7 @@ void dissolveedges_exec(BMesh *bm, BMOperator *op)
 		BM_ITER(v, &viter, bm, BM_VERTS_OF_MESH, NULL) {
 			if (BMO_elem_flag_test(bm, v, VERT_MARK)) {
 				if (BM_vert_edge_count(v) == 2) {
-					BM_vert_collapse_edges(bm, v->e, v);
+					BM_vert_collapse_edge(bm, v->e, v);
 				}
 			}
 		}
@@ -550,7 +550,7 @@ void dissolvelimit_exec(BMesh *bm, BMOperator *op)
 			BMVert *v = (BMVert *)weight_elems[i].ele;
 			/* check twice because cumulative effect could disolve over angle limit */
 			if (BM_vert_edge_angle(bm, v) < angle_limit) {
-				BM_vert_collapse_edges(bm, v->e, v); /* join edges */
+				BM_vert_collapse_edge(bm, v->e, v); /* join edges */
 			}
 		}
 	}

source/blender/bmesh/tools/BME_bevel.c
@@ -475,7 +475,7 @@ static BMVert *BME_bevel_wire(BMesh *bm, BMVert *v, float value, int res, int UN
 
 		//void BM_vert_collapse_faces(BMesh *bm, BMEdge *ke, BMVert *kv, float fac, int calcnorm) {
 		//hrm, why is there a fac here? it just removes a vert
-		BM_vert_collapse_edges(bm, v->e, v);
+		BM_vert_collapse_edge(bm, v->e, v);
 	}
 
 	return v1;
@@ -527,14 +527,14 @@ static BMLoop *BME_bevel_edge(BMesh *bm, BMLoop *l, float value, int UNUSED(opti
 			ke = kl->e;
 			/* BMESH-TODO: jfke doesn't handle customdata */
 			jf = bmesh_jfke(bm, kl->prev->radial_next->f, kl->f, kl->prev->e);
-			BM_vert_collapse_edges(bm, ke, kv);
+			BM_vert_collapse_edge(bm, ke, kv);
 		}
 		else {
 			BM_face_split(bm, kl->f, kl->next->next->v, kl->v, &nl, kl->next->e);
 			ke = kl->e;
 			/* BMESH-TODO: jfke doesn't handle customdata */
 			jf = bmesh_jfke(bm, kl->next->radial_next->f, kl->f, kl->next->e);
-			BM_vert_collapse_edges(bm, ke, kv);
+			BM_vert_collapse_edge(bm, ke, kv);
 		}
 		/* find saved loop pointer */
 		l = se->l;
@@ -573,14 +573,14 @@ static BMLoop *BME_bevel_edge(BMesh *bm, BMLoop *l, float value, int UNUSED(opti
 			ke = kl->e;
 			/* BMESH-TODO: jfke doesn't handle customdata */
 			jf = bmesh_jfke(bm, kl->prev->radial_next->f, kl->f, kl->prev->e);
-			BM_vert_collapse_edges(bm, ke, kv);
+			BM_vert_collapse_edge(bm, ke, kv);
 		}
 		else {
 			BM_face_split(bm, kl->f, kl->next->next->v, kl->v, &nl, kl->next->e);
 			ke = kl->e;
 			/* BMESH-TODO: jfke doesn't handle customdata */
 			jf = bmesh_jfke(bm, kl->next->radial_next->f, kl->f, kl->next->e);
-			BM_vert_collapse_edges(bm, ke, kv);
+			BM_vert_collapse_edge(bm, ke, kv);
 		}
 		/* find saved loop pointer */
 		l = se->l;

commit 014936b78a9e98a6d2ed40ee5462a1d975f2f4bf

fix for own regression in r44361 (broke BM_vert_in_face)
also fix py api: bmesh.utils.face_split(face, v1, v2)


git-svn-id: https://svn.blender.org/svnroot/bf-blender/trunk/blender@44428 954f8c5b-7b00-dc11-b283-0030488c597c

source/blender/bmesh/intern/bmesh_queries.c
@@ -101,7 +101,26 @@ BMLoop *BM_face_other_loop(BMEdge *e, BMFace *f, BMVert *v)
 
 int BM_vert_in_face(BMFace *f, BMVert *v)
 {
-	return bmesh_radial_find_first_faceloop(BM_FACE_FIRST_LOOP(f), v) != NULL;
+	BMLoop *l_iter, *l_first;
+
+#ifdef USE_BMESH_HOLES
+	BMLoopList *lst;
+	for (lst = f->loops.first; lst; lst = lst->next)
+#endif
+	{
+#ifdef USE_BMESH_HOLES
+		l_iter = l_first = lst->first;
+#else
+		l_iter = l_first = f->l_first;
+#endif
+		do {
+			if (l_iter->v == v) {
+				return TRUE;
+			}
+		} while ((l_iter = l_iter->next) != l_first);
+	}
+
+	return FALSE;
 }
 
 /*
@@ -551,7 +570,7 @@ float BM_edge_face_angle(BMesh *UNUSED(bm), BMEdge *e)
 		return angle_normalized_v3v3(l1->f->no, l2->f->no);
 	}
 	else {
-		return (float)M_PI / 2.0f; /* acos(0.0) */
+		return DEG2RADF(90.0f);
 	}
 }
 
@@ -581,7 +600,7 @@ float BM_vert_edge_angle(BMesh *UNUSED(bm), BMVert *v)
 		return M_PI - angle_v3v3v3(v1->co, v->co, v2->co);
 	}
 	else {
-		return (float)M_PI / 2.0f; /* acos(0.0) */
+		return DEG2RADF(90.0f);
 	}
 }
 

source/blender/python/bmesh/bmesh_py_types.c
@@ -708,6 +708,9 @@ PyDoc_STRVAR(bpy_bmvert_calc_edge_angle_doc,
 ".. method:: calc_edge_angle()\n"
 "\n"
 "   Return the angle between 2 connected edges.\n"
+"\n"
+"   :return: The angle between both edges in radians.\n"
+"   :rtype: float\n"
 );
 static PyObject *bpy_bmvert_calc_edge_angle(BPy_BMVert *self)
 {

source/blender/python/bmesh/bmesh_py_utils.c
@@ -327,7 +327,7 @@ static PyObject *bpy_bm_utils_face_split(PyObject *UNUSED(self), PyObject *args)
 	BMFace *f_new = NULL;
 	BMLoop *l_new = NULL;
 
-	if (!PyArg_ParseTuple(args, "O!O!|O!:face_split",
+	if (!PyArg_ParseTuple(args, "O!O!O!|O!:face_split",
 	                      &BPy_BMFace_Type, &py_face,
 	                      &BPy_BMVert_Type, &py_vert_a,
 	                      &BPy_BMVert_Type, &py_vert_b,
@@ -362,7 +362,7 @@ static PyObject *bpy_bm_utils_face_split(PyObject *UNUSED(self), PyObject *args)
 	bm = py_face->bm;
 
 	f_new = BM_face_split(bm, py_face->f,
-	                      py_vert_a->v, py_vert_a->v,
+	                      py_vert_a->v, py_vert_b->v,
 	                      &l_new, py_edge_example ? py_edge_example->e : NULL);
 
 	if (f_new && l_new) {
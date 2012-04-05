a3a6ef5b607422e73ea7600f12b3b75d862fdad5
SAmbler <Develop@Shaneware.Biz>
2012-04-05 11:19:27 +0930

    undo earlier fix as it didn't pick it up in the merge

--- source/blenderplayer/bad_level_call_stubs/stubs.c
+++ source/blenderplayer/bad_level_call_stubs/stubs.c
@@ -120,7 +120,6 @@ void EDBM_mesh_load(struct Object *ob) {}
 void EDBM_mesh_make(struct ToolSettings *ts, struct Scene *scene, struct Object *ob) {}
 void EDBM_mesh_normals_update(struct BMEditMesh *em) {}
 void *g_system;
-void EDBM_mesh_normals_update(struct BMEditMesh *em) {}
 
 struct Heap* BLI_heap_new (void){return NULL;}
 void BLI_heap_free (struct Heap *heap, void *ptrfreefp) {}

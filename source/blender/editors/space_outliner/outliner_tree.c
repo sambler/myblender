0896b2d78f448057c802374c88125362af1d4f50
SAmbler <Develop@Shaneware.Biz>
2012-04-11 18:15:42 +0930

    Make outliner sort use case-insensitive compare

--- source/blender/editors/space_outliner/outliner_tree.c
+++ source/blender/editors/space_outliner/outliner_tree.c
@@ -1221,7 +1221,7 @@ static int treesort_alpha(const void *v1, const void *v2)
 	if (comp==1) return 1;
 	else if (comp==2) return -1;
 	else if (comp==3) {
-		comp= strcmp(x1->name, x2->name);
+		comp= BLI_strcasecmp(x1->name, x2->name);
 		
 		if ( comp>0 ) return 1;
 		else if ( comp<0) return -1;

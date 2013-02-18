49faf3ad631fe9189eb9a61772bf6c5f779d1292
sambler <Develop@Shaneware.Biz>
2013-02-18 13:38:47 +1030

    svn update

--- build_files/cmake/buildinfo.cmake
+++ build_files/cmake/buildinfo.cmake
@@ -14,7 +14,7 @@ if(EXISTS ${SOURCE_DIR}/.svn/)
 	endif()
 endif()
 
-set(MY_WC_REVISION "54370")
+set(MY_WC_REVISION "54620")
 
 # BUILD_PLATFORM and BUILD_PLATFORM are taken from CMake
 # but BUILD_DATE and BUILD_TIME are plataform dependant

c3aeeaf5058faa8c02e034609f47fa0af051fbac
sambler <Develop@Shaneware.Biz>
2013-04-08 15:58:01 +0930

    re-implement 10 render result slots

--- source/blender/makesdna/DNA_image_types.h
+++ source/blender/makesdna/DNA_image_types.h
@@ -80,7 +80,7 @@ typedef struct Image {
 	struct anim *anim;
 	struct RenderResult *rr;
 
-	struct RenderResult *renders[8]; /* IMA_MAX_RENDER_SLOT */
+	struct RenderResult *renders[10]; /* IMA_MAX_RENDER_SLOT */
 	short render_slot, last_render_slot;
 	
 	short ok, flag;
@@ -149,7 +149,7 @@ typedef struct Image {
 
 /* render */
 #define IMA_MAX_RENDER_TEXT		512
-#define IMA_MAX_RENDER_SLOT		8
+#define IMA_MAX_RENDER_SLOT		10
 
 /* gen_flag */
 #define IMA_GEN_FLOAT		1

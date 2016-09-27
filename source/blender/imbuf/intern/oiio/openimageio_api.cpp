f14227accd8da2f9d513187992866ffdadf52661
sambler <Develop@Shaneware.Biz>
2016-09-28 03:57:21 +0930

    fix build with oiio 1.7.x

--- source/blender/imbuf/intern/oiio/openimageio_api.cpp
+++ source/blender/imbuf/intern/oiio/openimageio_api.cpp
@@ -35,6 +35,9 @@
 #include "utfconv.h"
 #endif
 
+#include <openimageio_api.h>
+#include <OpenImageIO/imageio.h>
+
 extern "C"
 {
 #include "MEM_guardedalloc.h"
@@ -47,9 +50,6 @@ extern "C"
 #include "IMB_colormanagement.h"
 #include "IMB_colormanagement_intern.h"
 }
-
-#include <openimageio_api.h>
-#include <OpenImageIO/imageio.h>
 
 OIIO_NAMESPACE_USING
 
